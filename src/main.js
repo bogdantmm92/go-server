710ba3465bdfe8af55a33cb4c588b4190c960109
Bogdan Tirca <bogdantmm@MacBook-Pro.fios-router.home>
2017-03-12 21:47:15 -0400

    es6

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-var server = require('http').createServer();
+function handleRequest(request, response) {
+    response.end('It Works!! Path Hit: ' + request.url);
+		console.log('It Works!! Path Hit: ' + request.url);
+}
+
+var server = require('http').createServer(handleRequest);
 var io = require('socket.io')(server);
-io.on('connection', function(client){
+io.on('connection', (client) => {
 	console.log("connection");
-  client.on('event', function(data){
+  client.on('event', (data) => {
   	console.log("event");
   });
-  client.on('disconnect', function(){
+  client.on('disconnect', () => {
   	console.log("disconnect");
   });
 });
-server.listen(3000);
+server.listen(3000, "0.0.0.0");
