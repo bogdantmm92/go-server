2c19664daf2b1a56fb34ab461823d2cc63b17e67
Bogdan Tirca <bogdantmm@MacBook-Pro.fios-router.home>
2017-03-26 23:36:49 -0400

    Create game + move + tests

--- src/db.js
+++ src/db.js
@@ -1,4 +1,6 @@
+var Promise = require('promise');
 var mongoose = require('mongoose');
+mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
@@ -12,21 +14,41 @@ function connected() {
 }
 
 // Define Schema
+var internalDataSchema = mongoose.Schema({
+  current_room: String,
+  socket_id: String,
+});
+
 var userSchema = mongoose.Schema({
   name: String,
   rating: Number,
-  lastLoggedIn: Date
+  internal_data: internalDataSchema,
+  created_at: Date,
+  updated_at: Date
 });
 
 var moveSchema = mongoose.Schema({
   x: Number,
   y: Number,
-  time: Date
+  color: String,
+  created_at: Date,
 });
 var gameSchema = mongoose.Schema({
-  combinedId: String,
-  moves: [moveSchema]
+  ids: [String],
+  accepted_ids: [String],
+  moves: [moveSchema],
+  black: String,
+  white: String,
+  current_turn: String,
+  created_at: Date,
+  updated_at: Date
 });
 
 var User = mongoose.model('User', userSchema);
 exports.User = User;
+
+var Game = mongoose.model('Game', gameSchema);
+exports.Game = Game;
+
+var Move = mongoose.model('Move', moveSchema);
+exports.Move = Move;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
+var server = require('http').createServer(handleRequest);
+var io = require('socket.io')(server);
+var db = require('./db');
+var _ = require('lodash');
+
 function handleRequest(request, response) {
     response.end('It Works!! Path Hit: ' + request.url);
 		console.log('It Works!! Path Hit: ' + request.url);
 }
-
-var server = require('http').createServer(handleRequest);
-var io = require('socket.io')(server);
-var db = require('./db');
 
 setupSocket();
 server.listen(3000, "0.0.0.0");
@@ -17,51 +18,169 @@ function setupSocket() {
 function handleConnection(client) {
 	console.log("connection");
 
-	client.on('disconnect', () => handleDisconnect(client));
 	client.on('create_user', (cb) => handleCreateUser(client, cb));
 	client.on('login_user', (user, cb) => handleLoginUser(client, user, cb));
-	enterPendingGameRoom(client);
+  client.on('create_game', (user2, cb) => handleCreateGame(client, user2, cb));
+  client.on('join_game', (game) => handleJoinGame(client, game));
+  client.on('do_move', (game, move) => handleMove(client, game, move));
 }
 
-function handleDisconnect(client) {
-	console.log('disconnect');
+
+function handleMove(client, game, move) {
+  db.Game.findByIdAndUpdate(game._id, {
+    $push: {
+      'moves': createMove(move)
+    }
+  }, {new: true}).exec().then((game) => {
+    io.to(game._id).emit('move', game);
+  });
 }
 
-function enterPendingGameRoom(client) {
-	console.log('enterPendingGameRoom');
-	client.join('pending_game', () => {
-		io.to('pending_game').emit('joined_pending_game');
+function handleJoinGame(client, game, cb) {
+  console.log('handleJoinGame: ' + game._id);
+  db.User.findOne({
+    'internal_data.socket_id': client.id
+  }).exec().then((user1) => {
+    return db.Game.findByIdAndUpdate(game._id, {
+      $push: {
+        'accepted_ids': user1._id
+      }
+    }, {new: true}).exec();
+  }).then((game) => {
+    client.join(game._id, () => {
+      io.to(game._id).emit('game_start', game);
+    });
+  });
+}
+
+function createMove(move) {
+  var currentDate = new Date();
+  return new db.Move({
+    'x': move.x,
+    'y': move.y,
+    'color': move.color,
+    'created_at': currentDate
+  });
+}
+function createGame(user1, user2) {
+  var currentDate = new Date();
+  var ids = [user1._id, user2._id];
+  var randomIds = _.sampleSize(ids, 2);
+  return new db.Game({
+    'ids': ids,
+    'accepted_ids': [user1._id],
+    'moves': [],
+    'black': randomIds[1],
+    'white': randomIds[0],
+    'current_turn': 'white',
+    'created_at': currentDate,
+    'updated_at': currentDate,
+  });
+}
+
+function handleCreateGame(client, user2, cb) {
+  console.log('handleCreateGame: ' + user2._id);
+  user1Promise = db.User.findOne({
+    'internal_data.socket_id': client.id
+  }).exec();
+  user2Promise = db.User.findById(user2._id).exec();
+  var user1, user2, currentGame;
+  Promise.all([user1Promise, user2Promise]).then(([user1, user2]) => {
+    return createGame(user1, user2).save();
+  }).then((game) => {
+    client.join(game._id, () => {
+      cb(game);
+      io.to(user2.internal_data.socket_id).emit('game_invite_sent', game);
+    });
+  });
+}
+
+function getUsersInRoom(room) {
+  var minAgo = new Date ();
+  minAgo.setMinutes(minAgo.getMinutes() - 5);
+
+  return db.User.find({
+    'internal_data.current_room': room,
+    'updated_at': {
+      "$gte": minAgo
+    }
+  }).exec();
+}
+
+function enterLiveRoom(client, user) {
+  enterRoom(client, user, 'live_room').then(([client, user]) => {
+    return getUsersInRoom('live_room');
+  }).then((users) => {
+    io.to('live_room').emit('joined:live_room', users);
+  });
+}
+
+function enterRoom(client, user, room) {
+  console.log(user._id + " joined " + room);
+  return new Promise((resolve, reject) => {
+    client.join(room, () => {
+      db.User.findByIdAndUpdate(user._id, {
+        $set: {
+          'updated_at': new Date(),
+          'internal_data.current_room': room
+        }
+      }).exec()
+        .then((user) => resolve([client, user]))
+        .catch((reason) => reject(reason));
+    });
 	});
 }
+
+function handleLoginUserSuccess(client, user) {
+  enterLiveRoom(client, user);
+  client.on('disconnect', () => handleDisconnect(client, user));
+}
+
 function handleLoginUser(client, user, cb) {
   console.log('handleLoginUser: ' + user._id);
   db.User.findByIdAndUpdate(user._id, {
     $set: {
-      'lastLoggedIn': new Date()
+      'updated_at': new Date(),
+      'internal_data': {
+        'socket_id': client.id,
+        'current_room': ''
+      }
     }
-  }, (err, user) => {
+  }).exec().then((user) => {
     console.log(JSON.stringify(user));
-    if (err) {
-      console.log("failed to login user");
-      // cb(null);
-    } else {
-        cb(user);
-    }
+    cb(user);
+    handleLoginUserSuccess(client, user);
   });
 }
+
 function handleCreateUser(client, cb) {
+  var currentDate = new Date();
   var newUser = new db.User({
     'name': 'Bogdan_' + Date.now(),
     'rating': 1,
-    'lastLoggedIn': new Date()
+    'internal_data': {
+      'socket_id': client.id,
+      'current_room': ''
+    },
+    'created_at': currentDate,
+    'updated_at': currentDate,
   });
   console.log('handleCreateUser: ' + newUser);
-  newUser.save((err) => {
-    if (err) {
-      console.log("failed to create user:" + err);
-      // cb(null);
-    } else {
-      cb(newUser);
-    }
+  newUser.save().then((user) => {
+    console.log(JSON.stringify(user));
+    cb(user);
+    handleLoginUserSuccess(client, user);
   });
+}
+
+function handleDisconnect(client, user) {
+	console.log('disconnect');
+  db.User.findByIdAndUpdate(user._id, {
+    $set: {
+      'internal_data': {
+        'socket_id': '',
+        'current_room': ''
+      }
+    }
+  }).exec();
 }
